4497c2d095c6cdbabe2e67bb5b71b035fb8f35fe
Adrian Lafond <mail@adrianlafond.com>
2019-10-12 16:12:34 -0400

    serve index.html

--- server/server.js
+++ server/server.js
@@ -147,6 +147,10 @@ app.delete('/v1/todo/:id', (req, res) => {
   })
 })
 
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
+
 app.listen(3000, () => {
   console.log('listening on 3000')
 })
