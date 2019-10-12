2df190e09288db4be439519f0ff2a7d3cd93e240
Adrian Lafond <mail@adrianlafond.com>
2019-10-12 16:12:34 -0400

    serve index.html

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
+})
+
 app.listen(3000, () => {
   console.log('listening on 3000')
 })
