662f9f3595fe015306a5eeedcc6be80a02e4b523
Adrian Lafond <mail@adrianlafond.com>
2019-10-12 20:22:58 -0400

    serve json data

--- server/server.js
+++ server/server.js
@@ -153,6 +153,23 @@ app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/index.html`)
 })
 
+app.get('/v1/todos', (_req, res)=> {
+
+  // res.send({ foo: 'bar' })
+  fs.readFile('./data.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500)
+    } else {
+      try {
+        res.set('Content-Type', 'application/json')
+        res.send(JSON.parse(data).todos)
+      } catch (_error) {
+        res.sendStatus(500)
+      }
+    }
+  })
+})
+
 app.listen(3000, () => {
   console.log('listening on 3000')
 })
