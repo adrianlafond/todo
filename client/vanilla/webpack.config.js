7c6e98323e0845835d935219280541a782deba31
Adrian Lafond <mail@adrianlafond.com>
2019-10-13 16:58:06 -0400

    implement server (#1)
    * run npm init
    
    * add standard
    
    * serve index.html
    
    * copy static css assets
    
    * add nodemon
    
    * serve json data
    
    * remove comment line
    
    * successfully fetch json to vanilla client
    
    * change devtool to cheap-module-eval-source-map
    
    * change devtool to cheap-eval-source-map
    
    * add post, put, delete rest apis
    
    * accept json data on put

---
+++ client/vanilla/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'TODOs',
+    })
+  ]
+}
