f2887bb8734f0b5873c3318a7917a99f4e3b9394
Adrian Lafond <mail@adrianlafond.com>
2019-10-14 20:55:02 -0400

    create stub todos service

--- client/todo-angular/src/app/components/todos/todos.component.ts
+++ client/todo-angular/src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private todosServices: TodosService) { }
 
   ngOnInit() {
+    console.log(this.todosServices);
   }
 
 }
