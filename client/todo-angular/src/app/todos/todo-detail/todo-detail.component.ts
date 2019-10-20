6cfd1bc5839d4a9a2046a0253ba9a41401ea6874
Adrian Lafond <mail@adrianlafond.com>
2019-10-20 16:26:00 -0400

    update from detail page

--- client/todo-angular/src/app/todos/todo-detail/todo-detail.component.ts
+++ client/todo-angular/src/app/todos/todo-detail/todo-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
+import { switchMap, catchError, tap } from 'rxjs/operators';
 
 import { TodosService, Todo } from '../services/todos.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -13,6 +13,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class TodoDetailComponent implements OnInit {
   todo$: Observable<Todo>;
+  nonDirty: Todo;
   error: HttpErrorResponse;
 
   constructor(
@@ -24,6 +25,10 @@ export class TodoDetailComponent implements OnInit {
     this.todo$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
         this.todosService.getTodo(params.get('id'))),
+      tap(todo => {
+        this.nonDirty = { ...todo };
+        return todo;
+      }),
       catchError((error: HttpErrorResponse) => {
         this.error = error;
         return of(null);
@@ -31,4 +36,14 @@ export class TodoDetailComponent implements OnInit {
     );
   }
 
+  save(todo) {
+    const hasDiff = Object.keys(todo).some(key => todo[key] !== this.nonDirty[key]);
+    if (hasDiff) {
+      this.nonDirty = { ...todo };
+      this.todosService.updateTodo(todo).subscribe(
+        (data: Todo) => this.nonDirty = data,
+        (error: HttpErrorResponse) => this.error = error
+      );
+    }
+  }
 }
