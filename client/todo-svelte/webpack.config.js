c467bebd7b0887e2a2040375cbdc02b78e66a3a7
Adrian Lafond <mail@adrianlafond.com>
2019-10-22 19:58:59 -0400

    get simple app running

--- client/todo-svelte/webpack.config.js
+++ client/todo-svelte/webpack.config.js
@@ -1,6 +1,57 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-
-modules.exports = {
+module.exports = {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  entry: {
+    bundle: ['./src/index.js']
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
+  output: {
+    path: __dirname + '/public',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'TODOs',
+    })
+  ],
 }
