60935a24bd735ee7898db4a1b9cf3c5bb86f7163
Adrian Lafond <mail@adrianlafond.com>
2019-10-29 19:46:59 -0400

    add and reset

--- client/todo-svelte/src/components/TodoList.svelte
+++ client/todo-svelte/src/components/TodoList.svelte
@@ -1,31 +1,75 @@
+<style>
+.todos__title {
+  padding: 1rem;
+}
+</style>
+
 <script>
 // import Todos from '../services/todos';
 import { Todos } from 'todo-shared';
 
 let todos = [];
 let error = null;
-Todos.read()
-  .then(data => {
-    todos = data || [];
-    error = null;
-  })
-  .catch(err => {
-    todos = [];
-    error = err;
-  });
+
+function fetch() {
+  Todos.read()
+    .then(data => {
+      todos = data || [];
+      error = null;
+    })
+    .catch(err => {
+      todos = [];
+      error = err;
+    });
+}
+
+function add() {
+  Todos.create()
+    .then(data => {
+      todos.push(data);
+      todos = todos.slice(0);
+      error = null;
+    })
+    .catch(err => {
+      error = err;
+    });
+}
+
+function reset() {
+  Todos.reset()
+    .then(data => {
+      todos = data;
+      error = null;
+    })
+    .catch(err => {
+      error = err;
+    });
+}
+
+fetch();
 </script>
 
 
 <div class="app-page">
-<h1>Todo List</h1>
+  <h2 class="todos__title">Todo List</h2>
 
-<ul>
-{#each todos as todo}
-  <li>{todo.text}</li>
-{/each}
-</ul>
+  {#if error}
+  <p>{error}</p>
+  {:else}
+  <ul class="todos-list__list">
+    <!-- <app-todo-item
+      *ngFor="let todo of todos; let isLast = last"
+      [data]="todo"
+      [isLast]="isLast">
+    </app-todo-item> -->
+    {#each todos as todo}
+    <li>{todo.text}</li>
+    {/each}
+  </ul>
+  {/if}
 
-{#if error}
-<p>{error}</p>
-{/if}
+  <div class="todos-list__admin">
+    <button on:click={add}>Add new todo</button>
+    <button on:click={reset}>Reset all todos</button>
+  </div>
 </div>
