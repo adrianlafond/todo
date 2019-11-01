8d223040b43043a57e42f7f53c0740d4a5f0523e
Adrian Lafond <mail@adrianlafond.com>
2019-10-31 20:59:14 -0400

    make writable subscriber test

---
+++ client/todo-svelte/src/components/WritableTest.svelte
@@ -0,0 +1,21 @@
+<style>
+.container {
+  display: inline-block;
+  padding: 1rem;
+  margin: 1rem;
+  border: 1px solid black;
+  background: #DCDCDC;
+}
+</style>
+
+<script>
+import Preferences from '../services/preferences';
+
+let theme = '';
+
+Preferences.subscribe(data => theme = data.theme);
+</script>
+
+<div class='container'>
+  <p>Theme: <strong>{theme}</strong></p>
+</div>
